eeb01eadc6c3a9e6678cf0b1423cd8fdb7be2cbd
Xi Xia <triplextension@gmail.com>
2017-08-23 13:46:57 -0700

    baseActivity now implements ToolBar

--- app/src/main/java/com/xixia/appetizing/UI/BaseActivity.java
+++ app/src/main/java/com/xixia/appetizing/UI/BaseActivity.java
@@ -1,65 +1,44 @@
 package com.xixia.appetizing.UI;
 
-import android.app.ActionBar;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.TextView;
 
 import com.xixia.appetizing.R;
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    //this will get the String name for the activity that is active
+//    String activityName = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+    }
 
-//        //this will get the String name for the activity that is active
-//        String activityName = getClass().getSimpleName();
-//        //use activityName to change what happens here
-//        if (activityName.equals(LoginActivity.class.getSimpleName())){
-//            getSupportActionBar().setCustomView(R.layout.abs_layout_login);
-//            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-//        } else {
-//            getSupportActionBar().setCustomView(R.layout.abs_layout_generic);
-//            TextView barTitle = ((TextView)getSupportActionBar().getCustomView().findViewById(R.id.actionBarTitle));
-//            if (activityName.equals(MakeSelectionActivity.class.getSimpleName())){
-//                barTitle.setText(getResources().getString(R.string.make));
-//            } else if (activityName.equals(CarSelectionActivity.class.getSimpleName())){
-//                barTitle.setText(getResources().getString(R.string.year_and_model));
-//            } else if (activityName.equals(BonusOfferingsActivity.class.getSimpleName())){
-//                barTitle.setText(getResources().getString(R.string.reward_details));
-//            }
-//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        }
-//
-//        getSupportActionBar().getCustomView().findViewById(R.id.action_bar_settings).setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent (getBaseContext(), SettingsActivity.class);
-//                startActivity(intent);
-//                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
-//            }
-//        });
+    protected boolean useToolbar() {
+        return true;
     }
 
     @Override
-    public void onBackPressed(){
-        super.onBackPressed();
-        overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+    public void setContentView(int layoutResID) {
+        View view = getLayoutInflater().inflate(layoutResID, null);
+        configureToolbar(view);
+        super.setContentView(view);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
-            return true;
+    private void configureToolbar(View view) {
+        toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            if (useToolbar()) {
+                setSupportActionBar(toolbar);
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            } else {
+                toolbar.setVisibility(View.GONE);
+            }
         }
-        return false;
     }
 }
