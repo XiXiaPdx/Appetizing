9db812637f556601f2becdab79927c1fa9a68792
Xi Xia <triplextension@gmail.com>
2017-08-22 20:26:36 -0700

    package structure for app

--- app/src/main/java/com/xixia/appetizing/UI/MainActivity.java
+++ app/src/main/java/com/xixia/appetizing/UI/MainActivity.java
@@ -1,10 +1,11 @@
-package com.xixia.appetizing;
+package com.xixia.appetizing.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.xixia.appetizing.R;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase mFireBaseDatabase;
