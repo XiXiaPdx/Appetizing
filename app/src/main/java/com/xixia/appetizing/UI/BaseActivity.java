163f2cff1b98b54c51efa0120cc338f884debfa0
Xi Xia <triplextension@gmail.com>
2017-08-23 15:27:18 -0700

    Toolbar is on different activities

--- app/src/main/java/com/xixia/appetizing/UI/BaseActivity.java
+++ app/src/main/java/com/xixia/appetizing/UI/BaseActivity.java
@@ -1,11 +1,14 @@
 package com.xixia.appetizing.UI;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.xixia.appetizing.R;
 
@@ -25,6 +28,23 @@ public abstract class BaseActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.overflow_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT)
+                        .show();
+                Intent intent = new Intent (getBaseContext(), SearchActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+                break;
+            default:
+                break;
+        }
         return true;
     }
 
---
+++ app/src/main/java/com/xixia/appetizing/UI/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.xixia.appetizing.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xixia.appetizing.R;
+
+public class SearchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
