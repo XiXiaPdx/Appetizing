a1b46b90a14e9fb9329092a4b0c821cf1be413fc
Xi Xia <triplextension@gmail.com>
2017-08-24 23:08:39 -0700

    refactored using query equalTo to check if new user profile needs to be created

--- app/src/main/java/com/xixia/appetizing/UI/MainActivity.java
+++ app/src/main/java/com/xixia/appetizing/UI/MainActivity.java
@@ -2,10 +2,7 @@ package com.xixia.appetizing.UI;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,7 +11,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.xixia.appetizing.Models.UserProfile;
 import com.xixia.appetizing.R;
@@ -37,7 +33,6 @@ public class MainActivity extends BaseActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser currentUser = firebaseAuth.getCurrentUser();
                 if (currentUser != null){
-                    Toast.makeText(MainActivity.this, currentUser.getUid(), Toast.LENGTH_SHORT).show();
                 } else {
                     startActivityForResult(
                             AuthUI.getInstance()
@@ -58,29 +53,28 @@ public class MainActivity extends BaseActivity {
         super.onActivityResult(request, result, data);
         if (request == RC_SIGN_IN) {
             if (result == RESULT_OK) {
-                //The single event listener is attached to child node with current user UID. If user is already in system, the listener will have a datasnapshot value that is NOT null. If a new user, the value will be null. Then, a user profile is created.
                 final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 final DatabaseReference mUserRef = mFireBaseDatabase.getReference(getString(R.string.user_node));
 
-                ValueEventListener checkForUser = new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    if (dataSnapshot.getValue() == null){
-                        String username = user.getDisplayName();
-                        String useremail = user.getEmail();
-                        String userUID = user.getUid();
-                        UserProfile newUser = new UserProfile(username, useremail, userUID);
-                        mUserRef.child(userUID).setValue(newUser);
+                //query for current user UID. If it exists, the snapshot will be NOT NULl. No new profile created. If null, new user and new profile created.
+                mUserRef.orderByChild("mUserUID").equalTo(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.getValue() == null){
+                            String username = user.getDisplayName();
+                            String useremail = user.getEmail();
+                            String userUID = user.getUid();
+                            UserProfile newUser = new UserProfile(username, useremail, userUID);
+                            mUserRef.child(userUID).setValue(newUser);
+                        }
                     }
-                }
 
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
 
                     }
-                };
+                });
 
-            mUserRef.child(user.getUid()).addListenerForSingleValueEvent(checkForUser);
 
             } else if (result == RESULT_CANCELED) {
                 finish();
