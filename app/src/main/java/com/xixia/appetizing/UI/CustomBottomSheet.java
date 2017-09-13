57229a7a1e1a4fa51c0ec37c1396b49e074134c4
Xi Xia <triplextension@gmail.com>
2017-09-12 20:52:21 -0700

    vibration switched to two finger. search button overlap splash pic

--- app/src/main/java/com/xixia/appetizing/UI/CustomBottomSheet.java
+++ app/src/main/java/com/xixia/appetizing/UI/CustomBottomSheet.java
@@ -25,25 +25,19 @@ import com.xixia.appetizing.R;
 public class CustomBottomSheet<V extends View> extends BottomSheetBehavior<V> {
     private Context mContext;
     private static ScaleGestureDetector mScaleDetector;
-    private static GestureDetector mLongPressListener;
     private  static Matrix matrix;
     private static View rootView;
-    private static Boolean settingOrigin;
     private static ImageView largeSplashPic;
     private static float scaleFactor = 1.f;
-    private float initialTouchY;
-    private float initialTouchX;
     private Boolean vibrated = false;
 
     public CustomBottomSheet(Context context) {
         super();
         mContext = context;
         mScaleDetector = new ScaleGestureDetector(mContext, new ScaleListener());
-        mLongPressListener = new GestureDetector(mContext, new LongPressListener());
         matrix = new Matrix();
         rootView = ((Activity)mContext).getWindow().getDecorView().findViewById(android.R.id.content);
         largeSplashPic = rootView.findViewById(R.id.largeSplashPic);
-        settingOrigin = true;
     }
 
     public CustomBottomSheet(Context context, AttributeSet attrs) {
@@ -54,7 +48,7 @@ public class CustomBottomSheet<V extends View> extends BottomSheetBehavior<V> {
     public boolean onInterceptTouchEvent(CoordinatorLayout parent, V child, MotionEvent event) {
         Log.d("MOTION", event.toString());
         // true sends the event to OnTouch, where UI changes should happen.
-       if(event.getPointerCount() == 2 || (event.getPointerCount() ==1 && (event.getEventTime() - event.getDownTime())> 200)) {
+       if(event.getPointerCount() == 2 ) {
            return true;
        }
        return false;
@@ -69,43 +63,22 @@ public class CustomBottomSheet<V extends View> extends BottomSheetBehavior<V> {
             switch (action) {
                 case MotionEvent.ACTION_POINTER_UP:
                     Log.d("POINTER UP", "POINTER UP");
-                    settingOrigin = true;
+                    vibrated = false;
                     break;
                 case MotionEvent.ACTION_UP:
                     Log.d("UP", " UP");
                     vibrated = false;
                     break;
             }
-//            if(action == MotionEvent.ACTION_MOVE && !mScaleDetector.isInProgress()){
-//                Log.d("MOVING", "YYY: " + mScaleDetector.getFocusY()  + "  XXX: " + mScaleDetector.getFocusX() );
-//
-//                final float newOriginY = initialTouchY - (mScaleDetector.getFocusY() - initialTouchY);
-//
-//                final float newOriginX = initialTouchX - (mScaleDetector.getFocusX() - initialTouchX);
-//
-//                matrix.setTranslate(newOriginX, newOriginY);
-//                largeSplashPic.setImageMatrix(matrix);
-//            }
 
-            if (event.getPointerCount() == 1  && event.getAction() != MotionEvent.ACTION_UP && event.getAction() != MotionEvent.ACTION_CANCEL && !vibrated) {
-                Log.d("Vibrate", "Vibate");
+            if (event.getPointerCount() == 2 && event.getAction() != MotionEvent.ACTION_UP && event.getAction() != MotionEvent.ACTION_CANCEL && event.getAction() != MotionEvent.ACTION_POINTER_UP && !vibrated) {
                 largeSplashPic.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                Log.d("Vibrate", "Vibate");
                 vibrated = true;
-            }  else if (event.getPointerCount() == 2) {
-                mScaleDetector.onTouchEvent(event);
             }
+            mScaleDetector.onTouchEvent(event);
             return true;
         } else return false;
-    }
-
-    private class LongPressListener extends GestureDetector.SimpleOnGestureListener{
-
-        @Override
-        public void onLongPress(MotionEvent e) {
-            super.onLongPress(e);
-            Log.d("LONG PRESS", "LONG PRESS");
-
-        }
     }
 
     private class ScaleListener
@@ -114,19 +87,16 @@ public class CustomBottomSheet<V extends View> extends BottomSheetBehavior<V> {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
             largeSplashPic.setScaleType(ImageView.ScaleType.MATRIX);
-            if(settingOrigin) {
-                //negative because what is under the finger IS NOT THE ORIGIN. But should be relative to it.
 
-                //this also sets the pivot point for scaling. So can scale from touch point
-                initialTouchY = detector.getFocusY();
-                initialTouchX = detector.getFocusX();
-                settingOrigin = false;
-            }
-            Log.d("ORIGIN", "YYY: "+ initialTouchY +"  XXX: "+ initialTouchX);
+            //this also sets the pivot point for scaling. So can scale from touch point
+            float scalePivotY = detector.getFocusY();
+            float scalePivotX = detector.getFocusX();
+
+            Log.d("SCALE PIVOT", "YYY: "+ scalePivotY +"  XXX: "+ scalePivotX);
 
             scaleFactor *= detector.getScaleFactor();
             scaleFactor = Math.max(1.0f, Math.min(scaleFactor, 5.0f));
-            matrix.setScale(scaleFactor, scaleFactor, initialTouchX , initialTouchY );
+            matrix.setScale(scaleFactor, scaleFactor, scalePivotX , scalePivotY );
             largeSplashPic.setImageMatrix(matrix);
             return true;
         }
