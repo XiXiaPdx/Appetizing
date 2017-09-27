fa1cc1452d47cbdceff7faeaa8c15065f422630b
Xi Xia <triplextension@gmail.com>
2017-09-26 20:05:03 -0700

    gitignore adjusted

--- app/src/main/java/com/xixia/appetizing/UI/SplashActivity.java
+++ app/src/main/java/com/xixia/appetizing/UI/SplashActivity.java
@@ -36,7 +36,6 @@ public class SplashActivity extends BaseActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
