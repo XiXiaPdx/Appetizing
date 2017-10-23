0200febcd33adea68f8d63aaa22cc5852b671451
Xi Xia <triplextension@gmail.com>
2017-10-23 14:55:33 -0700

    fixed SSL handshaked error

--- app/src/main/java/com/xixia/appetizing/Services/UnSplashServiceGenerator.java
+++ app/src/main/java/com/xixia/appetizing/Services/UnSplashServiceGenerator.java
@@ -1,10 +1,19 @@
 package com.xixia.appetizing.Services;
 
+import android.os.Build;
+import android.util.Log;
+
 import com.xixia.appetizing.Constants;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLContext;
+
+import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -36,10 +45,35 @@ public class UnSplashServiceGenerator {
             Class<S> serviceClass) {
         if (!httpClient.interceptors().contains(logging)) {
             httpClient.addInterceptor(logging);
-            builder.client(httpClient.build());
+            builder.client(enableTls12OnPreLollipop(httpClient).build());
             retrofit = builder.build();
         }
         return retrofit.create(serviceClass);
+    }
+
+    public static OkHttpClient.Builder enableTls12OnPreLollipop(OkHttpClient.Builder client) {
+        if (Build.VERSION.SDK_INT >= 16 && Build.VERSION.SDK_INT < 22) {
+            try {
+                SSLContext sc = SSLContext.getInstance("TLSv1.2");
+                sc.init(null, null, null);
+                client.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()));
+
+                ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                        .tlsVersions(TlsVersion.TLS_1_2)
+                        .build();
+
+                List<ConnectionSpec> specs = new ArrayList<>();
+                specs.add(cs);
+                specs.add(ConnectionSpec.COMPATIBLE_TLS);
+                specs.add(ConnectionSpec.CLEARTEXT);
+
+                client.connectionSpecs(specs);
+            } catch (Exception exc) {
+                Log.e("OkHttpTLSCompat", "Error while setting TLS 1.2", exc);
+            }
+        }
+
+        return client;
     }
 
 }
